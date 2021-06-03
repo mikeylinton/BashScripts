82b96142c7e2aa47bab358e6f2bbdeef2032c494
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-06-03 12:38:54 +0000

    Update BatStat.sh

--- BatStat.sh
+++ BatStat.sh
@@ -1,1 +1,3 @@
+upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -o "percentage:          *[0-9][0-9][0-9]%" | grep -o [0-9][0-9][0-9]%
 upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -o "percentage:          *[0-9][0-9]%" | grep -o [0-9][0-9]%
+upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -o "percentage:          *[0-9]%" | grep -o [0-9]%
